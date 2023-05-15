18c088410997b2cbce2846231cabd18131bcd208
Luis <lucho.lgrg04@gmail.com>
2023-05-15 17:43:37 -0500

    Inicianto la maquetación

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const video = document.querySelector('.video');
+const logoChibituber = document.querySelector('.logoChibituber');
+const nombreCanal = document.querySelector('.nombreCanal');
+
+
+document.addEventListener('DOMContentLoaded', e => {
+    fetchData()
+});
+
+
+
+
+
+
