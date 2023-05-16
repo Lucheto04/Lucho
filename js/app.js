cd99b60b90baf5e529a1879c450c1bfc92eb5ef2
lucho <lucho.lgrg04@gmail.com>
2023-05-15 22:07:33 -0500

    capturamos la api

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
-const video = document.querySelector('.video');
-const logoChibituber = document.querySelector('.logoChibituber');
-const nombreCanal = document.querySelector('.nombreCanal');
 
 
-document.addEventListener('DOMContentLoaded', e => {
-    fetchData()
-});
+async function bucarBoton()  {
+    const id = document.querySelector('.formu-input').value;
+    const url = `https://youtube138.p.rapidapi.com/video/details/?id=${id}&hl=en&gl=US`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '343f927d29msh160a667822bf493p182cbbjsne5b7b0fde73f',
+            'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 
-
-
-
-
+window.addEventListener('load', e => {
+    document.querySelector('.buscar').addEventListener('click', bucarBoton())
+})
