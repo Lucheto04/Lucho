1f04675afc7e8c664eb1376818ba856ecb0c7f80
Lucheto04 <124000317+Lucheto04@users.noreply.github.com>
2023-05-16 13:36:48 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,82 @@
+const options = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "343f927d29msh160a667822bf493p182cbbjsne5b7b0fde73f",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
 
+window.addEventListener("load", () => {
+    document.querySelector(".buscar").addEventListener("click", (e) => {
+        e.preventDefault();
+        const buscar = document.querySelector(".buscarBarra").value;
+        const url = `https://youtube138.p.rapidapi.com/search/?q=${buscar}`;
+        fetch(url, options)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                buscarId(data);
+            });
+    });
+});
 
-async function bucarBoton()  {
-    const id = document.querySelector('.formu-input').value;
-    const url = `https://youtube138.p.rapidapi.com/video/details/?id=${id}&hl=en&gl=US`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '343f927d29msh160a667822bf493p182cbbjsne5b7b0fde73f',
-            'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-        }
-    };
+function buscarId(data) {
+    // optenemos el Video
+    const ID = data.contents[1].video.videoId;
+    document.querySelector(
+        ".video"
+    ).src = `https://www.youtube-nocookie.com/embed/${ID}`;
 
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
+    // Obtenemos los videos relacionados
+    const ID2 = data.contents[2].video.videoId;
+    document.querySelector(".video2").src = `https://www.youtube-nocookie.com/embed/${ID2}`;
+
+    const ID3 = data.contents[3].video.videoId;
+    document.querySelector(".video3").src = `https://www.youtube-nocookie.com/embed/${ID3}`;
+
+    const ID4 = data.contents[4].video.videoId;
+    document.querySelector(".video4").src = `https://www.youtube-nocookie.com/embed/${ID4}`;
+
+    const ID5 = data.contents[5].video.videoId;
+    document.querySelector(".video5").src = `https://www.youtube-nocookie.com/embed/${ID5}`;
+
+    // obtenemos el Logo
+    const avatar = data.contents[1].video.author.avatar[0].url;
+    document.querySelector(".avatar").src = avatar;
+
+    // Traemos el Titulo
+    document.querySelector(".nombre_video").innerHTML = `<h3>${data.contents[1].video.title}</h3>`;
+
+    // Traemos el Nombre del Canal
+    document.querySelector(".nombre_canal").innerHTML = `<h3>${data.contents[1].video.author.title}</h3>`;
+
+    
+    document.querySelector(".descripcion").innerText = data.contents[1].video.descriptionSnippet;
+
+    // Traemos los comentarios 
+    const url_comentarios = `https://youtube138.p.rapidapi.com/video/comments/?id=${ID}&hl=en&gl=US`;
+    fetch(url_comentarios, options)
+    .then((respons) => respons.json())
+    .then((data) => {
+        // console.log(data);
+        comentarios(data)
+    });
+
+    const url_descripcion = `https://youtube138.p.rapidapi.com/video/details/?id=${ID}&hl=en&gl=US`;
+    fetch(url_descripcion, options)
+    .then((respons) => respons.json)
+}
+
+function comentarios(data) {
+    // console.log(data.comments);
+    let  acumulador_comentarios = '';
+    for (const valor of data.comments) {
+        // console.log(valor.content);
+        acumulador_comentarios += `<hr>${valor.content}
+        <hr>`;
     }
+    document.getElementById('cometarios').innerHTML = acumulador_comentarios
 }
 
 
-window.addEventListener('load', e => {
-    document.querySelector('.buscar').addEventListener('click', bucarBoton())
-})
+
